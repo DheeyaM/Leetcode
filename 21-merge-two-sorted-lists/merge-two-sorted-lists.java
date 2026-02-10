4a7a43dd04d4490b70d6fd56872ada8ec5ffe2f0
Dheeya Manilal <dheeyam7@gmail.com>
2026-02-10 16:21:59 +0200

    Time: 0 ms (100.00%) | Memory: 44.4 MB (54.63%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -13,26 +13,30 @@ class Solution {
         if (list1 == null){
             return list2;
         }
+
         if (list2 == null){
             return list1;
         }
+
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
 
         while (list1 != null && list2 != null){
             if (list1.val <= list2.val){
                 tail.next = list1;
-                list1 = list1.next; 
-            }else {
+                list1 = list1.next;
+            } else {
                 tail.next = list2;
                 list2 = list2.next;
             }
+
             tail = tail.next;
         }
 
         if (list1 == null){
             tail.next = list2;
-        } else if (list2 == null){
+        }
+        if (list2 == null){
             tail.next = list1;
         }
 
