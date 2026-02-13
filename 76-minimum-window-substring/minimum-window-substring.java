aaddc7a622bf2749e39fb636ddda7658152a920c
Dheeya Manilal <dheeyam7@gmail.com>
2026-02-13 14:00:20 +0200

    Time: 22 ms (27.22%) | Memory: 48 MB (21.82%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,39 +1,36 @@
 class Solution {
     public String minWindow(String s, String t) {
         Map<Character, Integer> window = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
-
-        if (t.length() == 0 || s.length() < t.length()){
+        Map<Character, Integer> tmap = new HashMap<>();
+        if (t.length() == 0 || t.length() > s.length()){
             return "";
         }
 
         for (int i = 0; i < t.length(); i++){
             char c = t.charAt(i);
-            if (!tMap.containsKey(c)){
-                tMap.put(c, 1);
-            }
-            else {
-                tMap.put(c, tMap.get(c) +1 );
+            if (!tmap.containsKey(c)){
+                tmap.put(c,1);
+            } else {
+                tmap.put(c, tmap.get(c) + 1);
             }
         }
 
-        int l = 0;
+
         int startIndex = 0;
-        int minLen = Integer.MAX_VALUE;
-        int required = tMap.size();
+        int l = 0;
+        int required = tmap.size();
         int satisfied = 0;
+        int minLen = Integer.MAX_VALUE;
 
         for (int r = 0; r < s.length(); r++){
             char c = s.charAt(r);
-            if (tMap.containsKey(c)){
-                if (!window.containsKey(c)){
-                    window.put(c,1);
-                } else {
-                    window.put(c, window.get(c) + 1);
-                }
+            if (!window.containsKey(c)){
+                window.put(c,1);
+            } else {
+                window.put(c, window.get(c) + 1);
             }
 
-            if (tMap.containsKey(c) && tMap.get(c).equals(window.get(c))){
+            if (tmap.containsKey(c) && tmap.get(c).equals(window.get(c))){
                 satisfied++;
             }
 
@@ -44,19 +41,17 @@ class Solution {
                     startIndex = l;
                 }
 
-                char remove = s.charAt(l);
-                if (tMap.containsKey(remove)){
-                    window.put(remove, window.get(remove) -1);
-
-                    if (window.get(remove) < tMap.get(remove)){
-    satisfied--;
-}
-
-                    
+                char removed = s.charAt(l);
+                if (tmap.containsKey(removed)){
+                    window.put(removed, window.get(removed) -1);
+                    if (window.get(removed) < tmap.get(removed)){
+                        satisfied--;
+                    }
                 }
                 l++;
-            }
 
+                
+            }
         }
         if (minLen == Integer.MAX_VALUE){
             return "";
