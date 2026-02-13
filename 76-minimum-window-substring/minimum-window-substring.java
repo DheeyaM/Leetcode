f8ebad886efdb55574ecfe0f4e29e6e79495ff8f
Dheeya Manilal <dheeyam7@gmail.com>
2026-02-13 19:34:39 +0200

    Time: 23 ms (24.13%) | Memory: 48.1 MB (16.33%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -2,31 +2,31 @@ class Solution {
     public String minWindow(String s, String t) {
         Map<Character, Integer> window = new HashMap<>();
         Map<Character, Integer> tmap = new HashMap<>();
-        if (t.length() == 0 || t.length() > s.length()){
+        if (t.length() == 0 || s.length() < t.length()){
             return "";
         }
 
-        for (int i = 0; i < t.length(); i++){
+        for (int i = 0; i < t.length() ; i++){
             char c = t.charAt(i);
             if (!tmap.containsKey(c)){
                 tmap.put(c,1);
             } else {
-                tmap.put(c, tmap.get(c) + 1);
+                tmap.put(c, tmap.get(c) +1);
             }
         }
 
-        int startIndex = 0;
-        int l = 0;
+        int start = 0;
+        int l =0;
         int minLen = Integer.MAX_VALUE;
-        int satisfied = 0;
         int required = tmap.size();
+        int satisfied = 0;
 
         for (int r = 0; r < s.length(); r++){
             char c = s.charAt(r);
             if (!window.containsKey(c)){
                 window.put(c,1);
             } else {
-                window.put(c, window.get(c) + 1);
+                window.put(c, window.get(c)+1);
             }
 
             if (tmap.containsKey(c) && tmap.get(c).equals(window.get(c))){
@@ -35,26 +35,28 @@ class Solution {
 
             while (satisfied == required){
                 int windowSize = r - l +1;
-                if (windowSize < minLen){
+                if (minLen > windowSize){
                     minLen = windowSize;
-                    startIndex = l;
+                    start = l;
                 }
 
                 char removed = s.charAt(l);
                 if (tmap.containsKey(removed)){
                     window.put(removed, window.get(removed) -1);
+                
 
-                    if (window.get(removed) < tmap.get(removed)){
-                        satisfied--;
-                    }   
+                if (tmap.get(removed) > window.get(removed)){
+                    satisfied--;
                 }
-                l++;
             }
-
+            l++;
+            }
         }
         if (minLen == Integer.MAX_VALUE){
             return "";
         }
-        return s.substring(startIndex, startIndex + minLen);
+
+        return s.substring(start, start + minLen);
+
     }
 }
