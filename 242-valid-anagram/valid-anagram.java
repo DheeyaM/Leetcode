68b19ce666ac8c85dab7d1bf8bc8b389fe7931ad
Dheeya Manilal <dheeyam7@gmail.com>
2026-03-02 11:02:42 +0200

    Time: 3 ms (91.70%) | Memory: 44.5 MB (75.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,23 +3,23 @@ class Solution {
         if (t.length() != s.length()){
             return false;
         }
-
         int[] count = new int[26];
         for (int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            count[c -'a']++;
-        }
-        for (int k = 0; k < t.length(); k++){
-            char c = t.charAt(k);
-            count[c -'a']--;
+            count[c - 'a']++;
         }
 
-        for (int j = 0; j < count.length; j++){
-            if (count[j] != 0){
-                return false;
-            }
+        for (int j = 0; j < t.length(); j++){
+            char c = t.charAt(j);
+            count[c - 'a']--;
         }
 
-        return true;
+for (int k = 0; k < count.length; k++){
+    if (count[k] != 0){
+        return false;
+    }
+}
+return true;
+       
     }
 }
